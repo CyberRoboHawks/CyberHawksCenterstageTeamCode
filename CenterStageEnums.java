96bd49240da9aa14d9f0b3aa7fc66f0f4ff8b8db
iceman <danheitzmann@gmail.com>
2023-10-29 17:38:28 -0400

    Added AprilTags to the enums

--- CenterStageEnums.java
+++ CenterStageEnums.java
@@ -1,6 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
 public class CenterStageEnums {
+
+    public enum AprilTag{
+        BlueLeft(1),
+        BlueCenter(2),
+        BlueRight(3),
+        RedLeft(4),
+        RedCenter(5),
+        RedRight(6);
+        private int value;
+        AprilTag(int value){
+            this.value=value;
+        }
+        public int getValue() {
+            return value;
+        }
+    }
+
     public enum ArmDirection {
         Down,
         None,
@@ -24,11 +41,16 @@ public class CenterStageEnums {
         Right,
         Left
     }
+
     public enum TapeColor {
         Blue,
         Red,
         Nothing
     }
 
-
+    public enum TapeLocation{
+        Left,
+        Center,
+        Right
+    }
 }
