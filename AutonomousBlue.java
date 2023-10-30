b2ab49a88760f3ec5e1eea9c622452748d4920f4
iceman <danheitzmann@gmail.com>
2023-10-30 15:29:39 -0400

    Auton blue

---
+++ AutonomousBlue.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "AutonomousBlue", group = "Pushbot", preselectTeleOp="TeleOpDrive")
+public class AutonomousBlue extends AutonomousBase {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize the hardware variables.
+        startupInit();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            executeOperations(CenterStageEnums.TapeColor.Blue);
+            sleep(30000);
+        }
+    }
+}
